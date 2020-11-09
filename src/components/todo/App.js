a2b4e33e7e04f8849053b2b39aef4b521c77b1c3
Joshua Escarilla <joshua.escarilla@sun-asterisk.com>
2020-11-09 18:49:26 +0800

    add scroll-more functionality(partial)

--- src/components/todo/App.js
+++ src/components/todo/App.js
@@ -1,47 +1,79 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import Create from './Create'
-import TodoList from './TodoList'
+import Create from "./Create";
+import TodoList from "./TodoList";
 
-import { http } from '../../api/http_service'
+import { http } from "../../api/http_service";
+
+import InfiniteScroll from "react-infinite-scroller";
 
 const App = (params) => {
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [maxPage, setMaxPage] = useState(0);
 
-    const fetchTodos = async (params) => {
-        const { data } = await http().get('todo');
-        setTodos(data.data);
-    }
-    
-    useEffect(() => {
-        fetchTodos();
-    }, []);
+  const fetchTodos = async (params) => {
+    const { data } = await http().get("todo");
 
-    const addNewTodo = (todo) => {
-        setTodos([ todo, ...todos ]);
-    }
+    setTodos(data.data);
+    setMaxPage(data.last_page);
+  };
 
-    const removeTodo = (todoId) => {
-        setTodos(todos.filter((todo)=>(todo.task_id !== todoId)));
-    }
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
-    const updateTodo = (updatedTodo) => {
-        const newTodos = todos.map(todo => {
-            if (todo.task_id === updatedTodo.task_id) {
-              return updatedTodo;
-            }
-            return todo;
-          });
-       
-          setTodos(newTodos);
-    }
+  const addNewTodo = (todo) => {
+    setTodos([todo, ...todos]);
+  };
 
-    return (
-        <div className="container mt-5">
-            <Create todoAdded={addNewTodo} />
-            <TodoList todos={todos} todoRemoved={removeTodo} todoEdited={updateTodo}/>
-        </div>
-    );
-}
+  const removeTodo = (todoId) => {
+    setTodos(todos.filter((todo) => todo.task_id !== todoId));
+  };
+
+  const updateTodo = (updatedTodo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.task_id === updatedTodo.task_id) {
+        return updatedTodo;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  const loadFunc = async (page) => {
+    if (page <= maxPage) {
+      const { data } = await http().get(`todo?page=${page}`);
+      const newTodos = todos;
+      Object.keys(data.data).forEach(function (key) {
+        newTodos.push(data.data[key]);
+      });
+      setTodos([...todos, newTodos]);
+    } else return;
+  };
+
+  return (
+    <div className="container mt-5">
+      <Create todoAdded={addNewTodo} />
+
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={loadFunc}
+        hasMore={true || false}
+        loader={
+          <div className="loader" key={0}>
+            Loading ...
+          </div>
+        }
+      >
+        <TodoList
+          todos={todos}
+          todoRemoved={removeTodo}
+          todoEdited={updateTodo}
+        />
+      </InfiniteScroll>
+    </div>
+  );
+};
 
 export default App;
